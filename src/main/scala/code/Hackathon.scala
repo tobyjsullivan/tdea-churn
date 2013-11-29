bf779e5aed6853c533d815ac34cb71816f36140d
Toby Sullivan <tobyjsullivan@gmail.com>
2013-11-28 23:19:19 -0800

    Updated start val

--- src/main/scala/code/Hackathon.scala
+++ src/main/scala/code/Hackathon.scala
@@ -6,7 +6,7 @@ object Hackathon extends App {
   val locked = "EQ7fIpT7i/Q="
   val res = "123456"
     
-  val startVal = 194070000000L
+  val startVal = 195807001737L
   val uberMaxVal = 281474976710656L
   
   val system = ActorSystem("hackathon")
